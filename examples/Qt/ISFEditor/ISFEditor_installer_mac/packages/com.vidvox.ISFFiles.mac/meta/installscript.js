db62dfa8e6bcef82434ce34b6387ae8cd2ca8fe7
ray cutler <ray@vidvox.net>
2019-11-11 14:25:58 -0500

    work on isf editor
    set permissions of installed ISF files to writable :: version bump

--- examples/Qt/ISFEditor/ISFEditor_installer_mac/packages/com.vidvox.ISFFiles.mac/meta/installscript.js
+++ examples/Qt/ISFEditor/ISFEditor_installer_mac/packages/com.vidvox.ISFFiles.mac/meta/installscript.js
@@ -11,4 +11,8 @@ Component.prototype.createOperationsForArchive = function(archive)
     }
     //	extract this component (the files, and only the files) to the folder
     component.addElevatedOperation("Extract", archive, "/Library/Graphics/ISF");
+    //  make the files in the directory writable
+    component.addElevatedOperation("Execute", "chmod", "-R", "u+w", "/Library/Graphics/ISF");
+    component.addElevatedOperation("Execute", "chmod", "-R", "g+w", "/Library/Graphics/ISF");
+    component.addElevatedOperation("Execute", "chmod", "-R", "o+w", "/Library/Graphics/ISF");
 }
--- examples/Qt/ISFEditor/ISFEditor_installer_win/packages/com.vidvox.ISFFiles.win/meta/installscript.js
+++ examples/Qt/ISFEditor/ISFEditor_installer_win/packages/com.vidvox.ISFFiles.win/meta/installscript.js
@@ -12,9 +12,15 @@ Component.prototype.createOperationsForArchive = function(archive)
     if (!installer.fileExists(tmpPath)) {
         component.addOperation("Mkdir", tmpPath);
     }
+
+    //console.log("****** TEST OUTPUT ******");
+
     component.addOperation("Extract", archive, tmpPath);
 
     if (systemInfo.productType === "windows")    {
+        //component.addOperation("Execute", "cmd", ["/C", "icacls", "C:\\ProgramData\\ISF", "/grant", "Users:F", "/t"]);
+        component.addOperation("Execute", "cmd", ["/C", "icacls", tmpPath, "/grant", "Users:F", "/t"]);
+
         component.addOperation("CreateShortcut",
                                tmpPath,
                                "@StartMenuDir@/ISF Files.lnk",
