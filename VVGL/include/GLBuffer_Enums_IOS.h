9adf62796acf4f50a4dcf927ff0c3506c645a7ed
Baku Hashimoto <mail@baku89.com>
2022-08-23 17:44:16 +0900

    added unsigned-short support

--- VVGL/include/GLBuffer_Enums_IOS.h
+++ VVGL/include/GLBuffer_Enums_IOS.h
@@ -12,6 +12,7 @@ enum InternalFormat	{
 	IF_R = GL_RED,
 	IF_RGB = GL_RGB,
 	IF_RGBA = GL_RGBA,
+	IF_RGBA16 = GL_RGBA16,
 	IF_RGBA32F = GL_RGBA32F,	//!<	four channel, 32 bit per channel.  probably can't render to this in iOS.
 	IF_RGBA16F = GL_RGBA16F_EXT,	//!<	four channel, 16 bit per channel.
 	IF_Depth24 = GL_DEPTH_COMPONENT24,
@@ -26,6 +27,7 @@ enum PixelFormat	{
 enum PixelType	{
 	PT_Float = GL_FLOAT,
 	PT_UByte = GL_UNSIGNED_BYTE,
+	PT_UShort = GL_UNSIGNED_SHORT,
 	PT_HalfFloat = GL_HALF_FLOAT,
 	PT_UShort88 = GL_UNSIGNED_SHORT_8_8_APPLE
 };
--- VVGL/include/GLBuffer_Enums_RPI.h
+++ VVGL/include/GLBuffer_Enums_RPI.h
@@ -8,6 +8,7 @@ enum InternalFormat	{
 	IF_None = 0,
 	IF_RGB = GL_RGB,
 	IF_RGBA = GL_RGBA,
+	IF_RGBA16 = GL_RGBA16,
 	IF_Depth16 = GL_DEPTH_COMPONENT16,
 };
 enum PixelFormat	{
@@ -20,5 +21,6 @@ enum PixelFormat	{
 enum PixelType	{
 	PT_Float = GL_FLOAT,
 	PT_UByte = GL_UNSIGNED_BYTE,
+	PT_UShort = GL_UNSIGNED_SHORT,
 	PT_UShort88 = GL_UNSIGNED_SHORT_8_8_APPLE
 };
